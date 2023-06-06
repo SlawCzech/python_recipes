331aa3afb1008d0e66f31c4225e039b577c22971
SlawCzech <slaw.czech@gmail.com>
2023-06-06 18:58:41 +0200

    update functions

--- functions/functions.py
+++ functions/functions.py
@@ -8,19 +8,22 @@ def add(a, b):
 # def - keyword (słowo klucz)
 # add - identyfikator (nazwa funkcji) zgodny z PEP8. Małe litery, słowa rozdzielone underscore, nie może zaczynać się od
 # cyfry, dopuszczalne znaki: litery, cyfry, underscore. nazwa: musi być akcją (funkcja ma coś robić), deskryptywna
-# (ma opisywać co robi)
+# (ma opisywać, co robi), powinna być po angielsku
 # () - wymagane
 # : - wcięcie w większości przypadków, wyjątki: lambda, typowanie, klucz:wartość w słowniku, if
 # ciało funkcji — zbiór instrukcji
 # return, zwraca zawsze wynik operacji, która jest zaraz po jego prawej stronie. Domyślnie zwraca None. Funkcja może
 # zwrócić tylko jedną rzecz
+# deklarację funkcji tworzy się raz, ale wywołań może być wiele
 
 # wywołanie funkcji
 add(2, 4)
 
-# Funkcja to obiekt przechowuje pewne cechy i funkcjonalności. Zachowuje się jak obiekt: dot notation.
-# vars()
-# funkcje w python są First Class Citizen — oznacza, że są obiektami.
+# Funkcja jest obiektem
+# Obiekt przechowuje pewne cechy i funkcjonalności. Zachowuje się jak obiekt: dot notation.
+# vars(add) sprawdza właściwości obiektu
+# dir(add) sprawdza metody obiektu
+# funkcje w python są First Class Citizen — oznacza to, że są obiektami.
 # Higher Order Function — może przyjąć jako parametr deklarację innej funkcji lub zwrócić deklarację funkcji
 
 
@@ -28,12 +31,13 @@ def upper(text):
     return text.upper()
 
 
-# Higher Order Function (map, filter, reduce)
+# Higher Order Function (m.in. map, filter, reduce)
 def sentence(callback, text):
     return callback(text)
 
 
-# Kompozycja jeśli dany obiekt przyjmuje inny obiekt jako parametr, to możemy modyfikować działanie pierwszego obiektu
+# Kompozycja: jeśli dany obiekt przyjmuje inny obiekt jako parametr, to możemy modyfikować działanie pierwszego obiektu.
+# np. w klasach, gdy pole klasy może mieć przypisane obiekty różnych klas
 
 
 def reverse(text):
@@ -43,7 +47,7 @@ def reverse(text):
 def capitalize(text):
     return text.capitalize()
 
-
+# sentence będzie miał inne działanie w zależności od callbacka
 print(sentence(reverse, 'Ala ma kota'))
 print(sentence(capitalize, 'Ala ma kota'))
 print(sentence(reverse, sentence(capitalize, 'ala ma kota')))
