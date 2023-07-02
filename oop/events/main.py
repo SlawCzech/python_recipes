5e56b9b4dfb514f83f6eda618de6b656eb272a11
SlawCzech <slaw.czech@gmail.com>
2023-07-02 10:37:39 +0200

    main function

--- oop/events/main.py
+++ oop/events/main.py
@@ -1,13 +1,30 @@
 from oop.events.reminder import Reminder
 from oop.events.workshop import Workshop
+from oop.events.event import Event
 
 
-class EventCreator:
+class EventCreator(Event):
     def __new__(cls, *args, **kwargs):
-        if kwargs.get("type") == "music":
-            self = Workshop()
-        elif kwargs.get("type") == "video":
-            self = Reminder()
+        if kwargs.get("speaker"):
+            self = Workshop(*args, **kwargs)
+        elif kwargs.get("remind"):
+            self = Reminder(*args, **kwargs)
         else:
-            self = super().__new__(cls)
+            self = Event(*args, **kwargs)
         return self
+
+
+# event_one = EventCreator(start_date="2023-09-01, 10:00", title="Good event", duration=55, participants=["Brunhilde", "Gunhilde"])
+# print(event_one)
+# print(repr(event_one))
+# print(type(event_one).__name__)
+#
+# workshop_one = EventCreator(start_date="2023-09-01, 10:00", title="Good event", duration=55, participants=["Brunhilde", "Gunhilde"], speaker="Magister")
+# print(workshop_one)
+# print(repr(workshop_one))
+# print(type(workshop_one).__name__)
+#
+# reminder_one = EventCreator(start_date="2023-09-01, 10:00", title="Good event", duration=55, participants=["Brunhilde", "Gunhilde"], remind=True)
+# print(reminder_one)
+# print(repr(reminder_one))
+# print(type(reminder_one).__name__)
