de8e0cfa62f167f5896553fda967f6fb2f9b62a5
SlawCzech <slaw.czech@gmail.com>
2023-07-04 22:10:09 +0200

    sql crud plus joins dict

--- sql/console_sql_examples.sql
+++ sql/console_sql_examples.sql
@@ -1,36 +1,3 @@
-
-## psql
-
-\? - general help
-\q - quit
-\d - list tables
-\d table_name - info o tabeli
-\du - info o userach
-\dp - info o uprawnieniach
-
-
-CREATE TABLE my_table (first_col VARCHAR, second_col INTEGER);
-INSERT INTO my_table (first_col, second_col) VALUES ('kasia', 18);
-
-CREATE USER saek WITH PASSWORD 'pass';
-ALTER USER saek SUPERUSER;
-GRANT SELECT ON my_table TO saek;
-GRANT INSERT ON my_table TO saek;
-GRANT DELETE ON my_table TO saek;
-REVOKE DELETE ON my_table FROM saek;
-
-
-SELECT * FROM my_table;
-
-DELETE FROM my_table WHERE second_col = 18;
-
-CREATE USER jarek;
-
-ALTER USER jarek WITH PASSWORD 'elo';
-
-GRANT SELECT ON my_table TO jarek;
-
-
 SELECT * FROM postgres.public.my_table;
 
 INSERT INTO my_table (first_col, second_col) VALUES ('basia', 20) RETURNING first_col, second_col;
