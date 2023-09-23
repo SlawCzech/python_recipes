e6fbc83c2cc796a8b66284e396fa4885a4a0b42d
SlawCzech <slaw.czech@gmail.com>
2023-09-23 18:18:35 +0200

    examples

--- adv_python/value_vs_reference.py
+++ adv_python/value_vs_reference.py
@@ -23,7 +23,27 @@ print(x, y, z)  # zmieni się we wszystkich, bo tylko pierwszy poziom referencji
 z[0] = [1, 2]
 print(x, y, z)
 
-
 # Domyślna jest shallow copy, bo deep copy potrzebuje więcej zasobów, ale przede wszystkim używa rekurencji, a ta
 # nie wiadomo ile razy by się wykonała (mogą być głębokie zagnieżdżenia) oraz może nastąpić nieskończona pętla, gdy
 # obiekty linkują same do siebie.
+
+from copy import deepcopy
+
+z_deep = deepcopy(x)
+print(z_deep == x, z_deep is x)  # True False
+
+arr = [1, 2, 3]
+value = 42
+
+def magic(col, val):
+    col.append(val)
+    val += 10
+    print(col, val)
+
+
+magic(arr, value)
+print(arr, value)
+
+# [1, 2, 3, 42] 52 wykonanie funkcji zmieni wartości arr i val
+# [1, 2, 3, 42] 42 poza funkcją arr jest zmienione (bo referencje), ale value nie (bo inny scope)
+
