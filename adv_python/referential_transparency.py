5aed77beddd1a85c7a22a54a74f31bb0f460e788
SlawCzech <slaw.czech@gmail.com>
2023-10-10 22:11:50 +0200

    referential transparency

---
+++ adv_python/referential_transparency.py
@@ -0,0 +1,12 @@
+def do_sth():
+    print('Call API')
+    return 42
+
+
+# result = do_sth()
+result = 42
+print(result)
+
+
+# program działa inaczej jeśli wywołamy funkcję niż kiedy podstawimy tylko wynik funkcji
+# powyższy kod NIE spełnia referential transparency
